c05d4ba64d0ecaa385e9fc7ce45db8c968b46385
nex3z <litianxing9@gmail.com>
2017-11-12 12:22:48 +0800

    Update Room configuration for Kotlin.

--- data/src/main/java/com/nex3z/popularmoviekotlin/data/entity/discover/MovieEntity.kt
+++ data/src/main/java/com/nex3z/popularmoviekotlin/data/entity/discover/MovieEntity.kt
@@ -5,18 +5,18 @@ import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "movies")
 data class MovieEntity(
-        val poster_path: String = "",
-        val adult: Boolean = false,
-        val overview: String = "",
-        val release_date: String = "",
-        val genre_ids: List<Int> = emptyList(),
-        @field:PrimaryKey val id: Long = 0,
-        val original_title: String = "",
-        val original_language: String = "",
-        val title: String = "",
-        val backdrop_path: String = "",
-        val popularity: Double = 0.0,
-        val vote_count: Int = 0,
-        val video: Boolean = false,
-        val vote_average: Double = 0.0
+        var poster_path: String = "",
+        var adult: Boolean = false,
+        var overview: String = "",
+        var release_date: String = "",
+        var genre_ids: List<Int> = emptyList(),
+        @field:PrimaryKey var id: Long = 0,
+        var original_title: String = "",
+        var original_language: String = "",
+        var title: String = "",
+        var backdrop_path: String = "",
+        var popularity: Double = 0.0,
+        var vote_count: Int = 0,
+        var video: Boolean = false,
+        var vote_average: Double = 0.0
 )
