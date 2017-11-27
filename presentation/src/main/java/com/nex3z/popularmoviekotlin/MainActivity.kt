339ce6e4d42c0f42b961344c6a1ea5e6eb37a217
nex3z <litianxing9@gmail.com>
2017-11-27 19:53:51 +0800

    Add drawer.

--- presentation/src/main/java/com/nex3z/popularmoviekotlin/MainActivity.kt
+++ presentation/src/main/java/com/nex3z/popularmoviekotlin/MainActivity.kt
@@ -2,42 +2,31 @@ package com.nex3z.popularmoviekotlin
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
-import com.nex3z.popularmoviekotlin.R
 import com.nex3z.popularmoviekotlin.detail.MovieDetailActivity
 import com.nex3z.popularmoviekotlin.discover.DiscoverMovieFragment
 import com.nex3z.popularmoviekotlin.discover.MovieDetailNavigator
 import com.nex3z.popularmoviekotlin.domain.model.movie.MovieModel
+import com.nex3z.popularmoviekotlin.favourite.FavouriteMovieFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
         MovieDetailNavigator {
 
-    private var mDiscoverMovieFragment: DiscoverMovieFragment? = null
+    private var discoverMovieFragment: DiscoverMovieFragment? = null
+    private var favouriteMovieFragment: FavouriteMovieFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
+        setupDrawer()
 
         if (savedInstanceState == null) {
             showDiscoverMovies()
@@ -52,41 +41,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
+            R.id.nav_discover -> {
+                showDiscoverMovies()
             }
-            R.id.nav_gallery -> {
-
+            R.id.nav_favourite -> {
+                showFavouriteMovies()
             }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.nav_settings -> {
 
             }
         }
@@ -101,12 +65,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         startActivity(intent)
     }
 
+    private fun setupDrawer() {
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+        nav_view.menu.getItem(0).isChecked = true
+    }
+
     private fun showDiscoverMovies() {
-        if (mDiscoverMovieFragment == null) {
-            mDiscoverMovieFragment = DiscoverMovieFragment.newInstance()
+        if (discoverMovieFragment == null) {
+            discoverMovieFragment = DiscoverMovieFragment.newInstance()
         }
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.vg_main_container, mDiscoverMovieFragment)
+        transaction.replace(R.id.vg_main_container, discoverMovieFragment)
+        transaction.commit()
+    }
+
+    private fun showFavouriteMovies() {
+        if (favouriteMovieFragment == null) {
+            favouriteMovieFragment = FavouriteMovieFragment.newInstance()
+        }
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.vg_main_container, favouriteMovieFragment)
         transaction.commit()
     }
 }
