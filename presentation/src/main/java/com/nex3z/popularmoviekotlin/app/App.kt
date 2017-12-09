efb1a5204d4367ba0363f7e852d4d878f4a7c7bd
nex3z <litianxing9@gmail.com>
2017-12-09 11:04:23 +0800

    Use realm form local storage.

--- presentation/src/main/java/com/nex3z/popularmoviekotlin/app/App.kt
+++ presentation/src/main/java/com/nex3z/popularmoviekotlin/app/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Context
 import com.nex3z.popularmoviekotlin.BuildConfig
 import com.nex3z.popularmoviekotlin.UiThread
+import com.nex3z.popularmoviekotlin.data.local.MovieDaoFactory
 import com.nex3z.popularmoviekotlin.data.net.RestClient
 import com.nex3z.popularmoviekotlin.data.repository.MovieRepositoryImpl
 import com.nex3z.popularmoviekotlin.domain.PopMovieService
@@ -20,7 +21,8 @@ class App : Application() {
 
     private fun initService() {
         val restClient = RestClient(BuildConfig.API_KEY)
-        val repository = MovieRepositoryImpl(this, restClient)
+        val repository = MovieRepositoryImpl(
+                MovieDaoFactory.createRealmMovieDao(applicationContext), restClient)
         service = PopMovieService(repository, JobExecutor(), UiThread)
     }
 
