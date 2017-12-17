901799289de51c940b6614577c3f82f5449e9c02
nex3z <litianxing9@gmail.com>
2017-12-17 20:52:27 +0800

    Update utilities.

--- presentation/src/main/java/com/nex3z/popularmoviekotlin/util/GenreUtil.kt
+++ presentation/src/main/java/com/nex3z/popularmoviekotlin/util/GenreUtil.kt
@@ -5,29 +5,29 @@ import com.nex3z.popularmoviekotlin.app.App
 import com.nex3z.popularmoviekotlin.domain.model.movie.MovieModel
 
 fun MovieModel.Genre.getName(): String? {
-    val id : Int? = when (this.value) {
-        28 -> R.string.caption_genre_action
-        12 -> R.string.caption_genre_adventure
-        16 -> R.string.caption_genre_animation
-        35 -> R.string.caption_genre_comedy
-        80 -> R.string.caption_genre_crime
-        99 -> R.string.caption_genre_documentary
-        18 -> R.string.caption_genre_drama
-        10751 -> R.string.caption_genre_family
-        14 -> R.string.caption_genre_fantasy
-        36 -> R.string.caption_genre_history
-        27 -> R.string.caption_genre_horror
-        10402 -> R.string.caption_genre_music
-        9648 -> R.string.caption_genre_mystery
-        10749 -> R.string.caption_genre_romance
-        878 -> R.string.caption_genre_science_fiction
-        10770 -> R.string.caption_genre_tv_movie
-        53 -> R.string.caption_genre_thriller
-        10752 -> R.string.caption_genre_war
-        37 -> R.string.caption_genre_western
-        else -> null
+    val id : Int = when (this) {
+        MovieModel.Genre.ACTION -> R.string.caption_genre_action
+        MovieModel.Genre.ADVENTURE -> R.string.caption_genre_adventure
+        MovieModel.Genre.ANIMATION -> R.string.caption_genre_animation
+        MovieModel.Genre.COMEDY -> R.string.caption_genre_comedy
+        MovieModel.Genre.CRIME -> R.string.caption_genre_crime
+        MovieModel.Genre.DOCUMENTARY -> R.string.caption_genre_documentary
+        MovieModel.Genre.DRAMA -> R.string.caption_genre_drama
+        MovieModel.Genre.FAMILY -> R.string.caption_genre_family
+        MovieModel.Genre.FANTASY -> R.string.caption_genre_fantasy
+        MovieModel.Genre.HISTORY -> R.string.caption_genre_history
+        MovieModel.Genre.HORROR -> R.string.caption_genre_horror
+        MovieModel.Genre.MUSIC -> R.string.caption_genre_music
+        MovieModel.Genre.MYSTERY -> R.string.caption_genre_mystery
+        MovieModel.Genre.ROMANCE -> R.string.caption_genre_romance
+        MovieModel.Genre.SCIENCE_FICTION -> R.string.caption_genre_science_fiction
+        MovieModel.Genre.TV_MOVIE -> R.string.caption_genre_tv_movie
+        MovieModel.Genre.THRILLER -> R.string.caption_genre_thriller
+        MovieModel.Genre.WAR -> R.string.caption_genre_war
+        MovieModel.Genre.WESTERN -> R.string.caption_genre_western
+        else -> -1
     }
-    return if (id != null) App.appContext.getString(id) else null
+    return if (id != -1) App.appContext.getString(id) else null
 }
 
 fun List<MovieModel.Genre>.getNames(): String =
