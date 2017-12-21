ef3981f8318a990440a5ed686e78939e4398ce0a
nex3z <litianxing9@gmail.com>
2017-12-21 20:53:32 +0800

    Add BaseParams.

---
+++ data/src/main/java/com/nex3z/popularmoviekotlin/data/entity/BaseParams.kt
@@ -0,0 +1,7 @@
+package com.nex3z.popularmoviekotlin.data.entity
+
+abstract class BaseParams {
+
+    abstract val params: Map<String, String>
+
+}
--- data/src/main/java/com/nex3z/popularmoviekotlin/data/entity/discover/DiscoverMovieParams.kt
+++ data/src/main/java/com/nex3z/popularmoviekotlin/data/entity/discover/DiscoverMovieParams.kt
@@ -1,25 +1,17 @@
 package com.nex3z.popularmoviekotlin.data.entity.discover
 
-class DiscoverMovieParams(page: Int, sortBy: SortBy) {
+import com.nex3z.popularmoviekotlin.data.entity.BaseParams
 
-    val params = mutableMapOf<String, String>()
+class DiscoverMovieParams(
+        val page: Int,
+        val sortBy: SortBy
+) : BaseParams() {
 
-    var page: Int = 1
-        set(value) {
-            field = value
-            params.put("page", value.toString())
-        }
-
-    var sortBy: SortBy = SortBy.RELEASE_DATE_DESC
-        set(value) {
-            field = value
-            params.put("sort_by", sortBy.value)
-        }
-
-    init {
-        this@DiscoverMovieParams.page = page
-        this@DiscoverMovieParams.sortBy = sortBy
-    }
+    override val params: Map<String, String>
+        get() = mapOf(
+                "page" to page.toString(),
+                "sort_by" to sortBy.value
+        )
 
     enum class SortBy(val value: String) {
         RELEASE_DATE_DESC("release_date.desc"),
